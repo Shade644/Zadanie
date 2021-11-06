85eef1be107a28d2a1b806af6b0e507290b0c8ae
Shade644 <adrianw507@wp.pl>
2021-11-06 13:10:24 +0100

    tak
    tak

---
+++ Zadanie1/Models/FormModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zadanie1.Models
+{
+
+    public class FormModel
+    {
+        [Display(Name = "Imie")]
+        public string Name { get; set; }
+        [Display(Name = "Ilość dni od twoich urodzin")]
+        public string Date { get; set; }
+        public string Date2 { get; set; }
+
+
+    }
+
+}
+    
+
